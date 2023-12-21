2b2f197503e62d60cbae2bb23c6926671ced1bfe
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-12-21 14:43:42 +0530

    controll

--- controller/email-controller.js
+++ controller/email-controller.js
@@ -1,3 +1,4 @@
+
 import Email from "../model/email.js"
 export const saveSentEmail = (request, response) => {
     try {
@@ -5,6 +6,20 @@ export const saveSentEmail = (request, response) => {
         email.save();
         response.status(200).json("email save successfully")
     } catch (error) {
+        response.status(500).json(error.message)
+    }
+}
+export const getEmails = async (request, response) => {
+    try {
+        let emails;
+        if (false) {
+           
+        }else{
+            emails = await Email.find({ type: request.params.type })
+        }
+        return response.status(200).json(emails)
+    } catch (error) {
+        console.log(error);
         response.status(500).json(error.message)
     }
 }
--- routes/route.js
+++ routes/route.js
@@ -1,5 +1,7 @@
 import express from "express";
-import { saveSentEmail } from "../controller/email-controller.js";
+import { saveSentEmail,getEmails } from "../controller/email-controller.js";
 const routes=express.Router();
 routes.post("/save",saveSentEmail)
+routes.get("/emails/:type",getEmails)
+routes.post("/save-draft",saveSentEmail)
 export default routes;
