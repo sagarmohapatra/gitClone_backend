7a00c158acf256e23faed7869e144af1e6f789f7
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-12-22 20:14:33 +0530

    all save

--- controller/email-controller.js
+++ controller/email-controller.js
@@ -12,14 +12,28 @@ export const saveSentEmail = (request, response) => {
 export const getEmails = async (request, response) => {
     try {
         let emails;
-        if (false) {
-           
-        }else{
+        if (request.params.type === "bin") {
+            emails = await Email.find({ bin: true })
+        }
+        else if (request.params.type === "allmail") {
+            emails = await Email.find({})
+        }
+        else {
             emails = await Email.find({ type: request.params.type })
         }
         return response.status(200).json(emails)
     } catch (error) {
         console.log(error);
         response.status(500).json(error.message)
+    }
+}
+
+export const moveEmailsToBin = async (request, response) => {
+    try {
+        await Email.updateMany({ _id: { $in: request.body } }, { $set: { bin: true, starred: false, type: "" } })
+        return response.status(200).json("email deleted")
+    } catch (error) {
+        console.log(error);
+        response.status(500).json(error.message);
     }
 }
--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,9 @@
 import express from "express";
-import { saveSentEmail,getEmails } from "../controller/email-controller.js";
+import { saveSentEmail,getEmails,moveEmailsToBin } from "../controller/email-controller.js";
 const routes=express.Router();
 routes.post("/save",saveSentEmail)
 routes.get("/emails/:type",getEmails)
 routes.post("/save-draft",saveSentEmail)
+routes.post("/bin", moveEmailsToBin)
+
 export default routes;
