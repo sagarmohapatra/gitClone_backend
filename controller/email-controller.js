9a716bead29cfc2f98a943ee89d5970da9eff39a
sagarmohapatra <sagarkumarmohapatra5@gamail.com>
2023-12-27 16:29:09 +0530

    last push of backend

--- controller/email-controller.js
+++ controller/email-controller.js
@@ -1,4 +1,5 @@
 
+import { request, response } from "express";
 import Email from "../model/email.js"
 export const saveSentEmail = (request, response) => {
     try {
@@ -15,8 +16,10 @@ export const getEmails = async (request, response) => {
         if (request.params.type === "bin") {
             emails = await Email.find({ bin: true })
         }
-        else if (request.params.type === "allmail") {
+        else if (request.params.type === "allmails") {
             emails = await Email.find({})
+        } else if (request.params.type === "starred") {
+            emails = await Email.find({ starred: true, bin: false })
         }
         else {
             emails = await Email.find({ type: request.params.type })
@@ -32,6 +35,24 @@ export const moveEmailsToBin = async (request, response) => {
     try {
         await Email.updateMany({ _id: { $in: request.body } }, { $set: { bin: true, starred: false, type: "" } })
         return response.status(200).json("email deleted")
+    } catch (error) {
+        console.log(error);
+        response.status(500).json(error.message);
+    }
+}
+export const toggleStarredEmails = async (request, response) => {
+    try {
+        await Email.updateOne({ _id: request.body.id }, { $set: { starred: request.body.value } })
+        return response.status(200).json("email is starred mark")
+    } catch (error) {
+        console.log(error);
+        response.status(500).json(error.message);
+    }
+}
+export const deleteEmails = async (request, response) => {
+    try {
+        await Email.deleteMany({ _id: { $in: request.body } })
+        return response.status(200).json("email delete successfully")
     } catch (error) {
         console.log(error);
         response.status(500).json(error.message);
